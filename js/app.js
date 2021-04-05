76ddc3f4e170ef0863bc5977f75d95ef14f1f266
AbdullahElian1 <abodelian28@gmail.com>
2021-04-05 13:32:38 +0300

    add the rest of quastion in js file

--- js/app.js
+++ js/app.js
@@ -20,4 +20,71 @@ switch(firstQuastion.toLowerCase()){
 
 }
 
+let secondQuastion = prompt("Is the age of the page 23 owner? answer yes/no");
+console.log(secondQuastion);
+switch(secondQuastion.toLowerCase()){
+    case 'yes' :
+    case 'y':
+        alert(" you are wrong it's 22");
+        break;
+    case 'no':
+    case 'n' :
+        alert("you are correct it's 22 ");
+        break;
+    default :
+    alert("please enter yes or no ");    
+
+}
+
+
+let thirdQuastion = prompt("Did Abdullah graduate from the Hashemite University? answer yes/no");
+console.log(thirdQuastion);
+switch(thirdQuastion.toLowerCase()){
+    case 'yes' :
+    case 'y':
+        alert("you are wrong it's from  the University of Jordan");
+        break;
+    case 'no':
+    case 'n' :
+        alert(" you are correct it's from  the University of Jordan ");
+        break;
+    default :
+    alert("please enter yes or no ");    
+
+}
+
+let fourthQuastion = prompt("Was his major in the university computer science? answer yes/no");
+console.log(fourthQuastion);
+switch(fourthQuastion.toLowerCase()){
+    case 'yes' :
+    case 'y':
+        alert("you are correct");
+        break;
+    case 'no':
+    case 'n' :
+        alert("you are wrong");
+        break;
+    default :
+    alert("please enter yes or no ");    
+
+}
+
+let fifthQuastion = prompt("Does he have a passion for programming? answer yes/no");
+console.log(fifthQuastion);
+switch(fifthQuastion.toLowerCase()){
+    case 'yes' :
+    case 'y':
+        alert("you are correct");
+        break;
+    case 'no':
+    case 'n' :
+        alert("you are wrong");
+        break;
+    default :
+    alert("please enter yes or no ");    
+
+}
+
+
+document.write("thank you "+ userName +" for visited my website ");
 
